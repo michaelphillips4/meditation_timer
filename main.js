81a111982f9238d412e1fd84a4fad14d5502f769
michaelp <michaelp@linux-laptop.lan>
2025-03-31 22:47:41 +0100

    added first draft of save option

--- main.js
+++ main.js
@@ -5,15 +5,11 @@ const timerElement = document.getElementById("timer");
 
 const _chime = new Audio("section.mp3");
 
+const storageKey = "SeriesTimerKey";
+
 let _timer;
 
-const add = () => {
-
-  const li = document.createElement("li");
-  li.appendChild(document.createElement("time-section"));
-  stagesList.appendChild(li);
-  document.getElementById("run-buttons").classList.remove("hide");
-};
+const add = () => addSection(10);
 
 const start = () => {
   _chime.play();
@@ -27,31 +23,23 @@ const stop = () => {
   displayTimer("");
 };
 
-const chime = () => {
-   _chime.play();
-};
+const chime = () => _chime.play();
 
 const displayTimer = (value) => (timerElement.innerHTML = value);
 
-const timeStateMessage = (time,sectionIndex,stages)=>
-{
-  const m =  Math.floor(time / 60);
-  const s = time % 60;
-
-   return `Section ${sectionIndex + 1} - 
+const timeStateMessage = (time, sectionIndex, stages) =>
+  `Section ${sectionIndex + 1} - 
        Duration ${stages[sectionIndex]} min - 
-       Time ${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
-  
+       Time ${(Math.floor(time / 60)).toString().padStart(2, '0')}:
+            ${(time % 60).toString().padStart(2, '0')}`;
 
-}
-
-
+const getStages = () => [...document.querySelectorAll(".time-range")].map((e) =>
+  parseInt(e.value)
+);
 
 const startSession = () => {
-  const stages = [...document.querySelectorAll(".time-range")].map((e) =>
-    parseInt(e.value)
-  );
-  const totalTime = stages.reduce((x, y) => x + y, 0) ;
+  const stages = getStages();
+  const totalTime = stages.reduce((x, y) => x + y, 0);
 
   let time = 0;
   let sectionIndex = 0;
@@ -59,9 +47,9 @@ const startSession = () => {
 
   this._timer = setInterval(() => {
     time++;
-   
-    displayTimer(timeStateMessage(time,sectionIndex,stages));
-       
+
+    displayTimer(timeStateMessage(time, sectionIndex, stages));
+
     if (time === (sectionTime * 60)) {
       sectionIndex++;
       sectionTime += parseInt(stages[sectionIndex]);
@@ -77,4 +65,78 @@ const startSession = () => {
 
   }, 1000);
 };
- 
+
+const save = () => {
+  const itemData = {};
+  itemData.name = document.getElementById("name").value;
+  itemData.stages = getStages();
+  const data = JSON.parse(localStorage.getItem(storageKey)) ?? [];
+  data.push(itemData);
+  localStorage.setItem(storageKey, JSON.stringify(data));
+  getSavedItems();
+}
+
+const showMeditation = () => {
+  document.getElementById("meditation").classList.remove("hide");
+}
+
+const showSavedMeditations = () => {
+  document.getElementById("savedMeditations").classList.remove("hide");
+}
+
+const showMeditationControls = () => {
+  document.getElementById("run-button").classList.remove("hide");
+}
+
+const getSavedItems = () => {
+  const data = JSON.parse(localStorage.getItem(storageKey)) ?? [];
+  let out = "";
+  let i = 0;
+  for (const item of data) {
+    out += `<li> <a href="#" onclick=load(${i})>Load</a> Name : ${item.name} ${item.stages} 
+            <i class="fa fa-trash section-remove right" onclick=remove(${i})></i>
+            <hr />
+            </li>`;
+    i++;
+  }
+  document.getElementById("saved").innerHTML = out;
+  if (out.length === 0) {
+    showMeditation();
+    document.getElementById("savedMeditations").classList.add("hide");
+  }
+  else {
+    showSavedMeditations();
+    document.getElementById("meditation").classList.add("hide");
+  }
+}
+
+getSavedItems();
+
+const load = (i) => {
+  stagesList.innerHTML = "";
+  const data = JSON.parse(localStorage.getItem(storageKey)) ?? [];
+  const item = data[i];
+  document.getElementById("name").value = item.name;
+  for (const duration of item.stages) {
+    addSection(duration);
+  }
+  showMeditation();
+}
+
+const addSection = (duration) => {
+  const li = document.createElement("li");
+  li.classList.add("section");
+  const section = document.createElement("time-section")
+  li.appendChild(section);
+  stagesList.appendChild(li);
+  section.querySelector(".time-range").value = duration;
+  section.querySelector(".section-time").innerHTML = duration;
+  document.getElementById("run-buttons").classList.remove("hide");
+}
+
+const remove = (i) => {
+  const data = JSON.parse(localStorage.getItem(storageKey)) ?? [];
+  data.splice(i, 1);
+  localStorage.setItem(storageKey, JSON.stringify(data));
+  getSavedItems()
+}
